96ce4d9bac90dd2017d5c57b209eace9e47eac12
dimmycore <flamekh@gmail.com>
2023-01-04 17:22:45 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,27 @@
+class AuthEmail:
+    email = 'flamekh@gmail.com'
+    password = 'abc12345'
+
+class AuthPhone:
+    phone = '9021383091'
+    password = 'abc12345'
+
+class AuthLogin:
+    login = 'rtkid_1670145210607'
+    password = 'abc12345'
+
+class InvalidData:
+    login = 'login_non_true'
+    password = "qwerty"
+
+class SymbolData:
+    login = '/*!@#$'
+    password = "qwerty"
+
+class KirillData:
+    login = 'кириллица'
+    password = "кириллица"
+
+class BIGData:
+    login = 'login'
+    password = "passw"
